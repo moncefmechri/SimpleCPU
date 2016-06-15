bfe1db5b9726eb5f47f0d296e1526f325fe5d7f4
Moncef Mechri <moncef.mechri@gmail.com>
2016-06-15 15:05:43 +0200

    Complete pintool

--- pin/pintracerecorder.cpp
+++ pin/pintracerecorder.cpp
@@ -19,11 +19,38 @@ unique_ptr<TraceRecorder> recorder;
 
 using namespace std;
 
+void log_memaccess(void* address)
+{
+    recorder->log_memaccess((uint64_t) address);
+}
+
+void log_instruction(void)
+{
+    recorder->log_instruction();
+}
+
 void fini(int code, void* v)
-{}
+{
+    recorder->close();
+}
 
 void instruction(INS ins, void* v)
-{}
+{    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR) log_instruction, IARG_END);
+
+    const int nb_mem_operands = INS_MemoryOperandCount(ins);
+    for (int mem_op = 0; mem_op < nb_mem_operands; ++mem_op)
+    {
+        if (INS_MemoryOperandIsRead(ins, mem_op))
+        {
+            INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) log_memaccess, IARG_MEMORYOP_EA, mem_op, IARG_END);
+        }
+        //An instruction could both read and write memory so we always test for both
+        if (INS_MemoryOperandIsWritten(ins, mem_op))
+        {
+            INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) log_memaccess, IARG_MEMORYOP_EA, mem_op, IARG_END);
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
