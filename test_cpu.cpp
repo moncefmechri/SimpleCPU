ca8c95058c6f65a1b7dd57c37a23f5216cb589c5
Moncef Mechri <moncef.mechri@gmail.com>
2016-08-04 14:57:38 +0200

    Make sure the two caches have the same number of sets

--- test_cpu.cpp
+++ test_cpu.cpp
@@ -5,8 +5,18 @@
 
 #include <libcachesim/CacheToolbox.h>
 
-
 using namespace std;
+
+
+/* Returns the L2 associativity needed in order for L2 to have the same number of sets
+ * than L1 */
+unsigned compute_L2_associativity(unsigned L1_size, unsigned L1_assoc, unsigned L2_size)
+{
+    const float ratio = L2_size / (float) L1_size;
+    cout << ratio << endl;
+    cout << "L2 assoc: " << L1_assoc * ratio << endl;
+    return L1_assoc * ratio;
+}
 
 int main(int argc, char** argv)
 {
@@ -20,7 +30,7 @@ int main(int argc, char** argv)
     const unsigned L2_size = stoul(argv[4]);
 
     const unsigned L1_assoc = 64;
-    const unsigned L2_assoc = 2 * L1_assoc;
+    const unsigned L2_assoc = compute_L2_associativity(L1_size, L1_assoc, L2_size);
 
     CacheConfig L1_config(L1_size, L1_assoc, 64);
     CacheConfig L2_config(L2_size, L2_assoc, 64);
